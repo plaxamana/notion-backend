5f83a761208872f3e003d98dd2c66dad415465fb
Philip Laxamana <philip.laxamana@hotmail.com>
2021-05-19 19:34:48 -0400

    installed cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const getEvents = require('./services/notion');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 const app = express()
+
+app.use(cors())
 
 app.get('/events', async (req, res) => {
   const events = await getEvents()
